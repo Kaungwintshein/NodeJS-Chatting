7164bc73a02b8ec42b9846c63bde5bc94b38282c
Kaung Wint Shein <k028176@gmail.com>
2025-02-09 03:10:05 +0630

    first commit

---
+++ utils/chatBot.js
@@ -0,0 +1,11 @@
+// const axios = require('axios');
+
+// export async function getChatResponse(message) {
+//     const response = await axios.post('http://localhost:11434/api/generate', {
+//         model: "deepseek-r1:1:5",
+//         prompt: message,
+//         stream: false
+//     });
+//     // console.log("AI Response => ", firstResponse.data.response);
+//     return response.data.response;
+// }
